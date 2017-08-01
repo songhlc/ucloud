5da59e7dae56d03a07a4fb3ae4f893b27c4defcf
windknow <songhlc@yonyou.com>
2017-08-01 08:05:11 +0800

    version 1.0.0 add box, boxcontent, boxstiky

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <demo></demo>
+  </div>
+</template>
+
+<script>
+import demo from './demo/demo'
+
+export default {
+  name: 'app',
+  components: {
+    demo
+  }
+}
+</script>
+
+<style>
+#app {
+
+}
+</style>
